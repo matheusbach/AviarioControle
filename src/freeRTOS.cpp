015e0d1c03901d4e29379c56bdc06cb56d16c5f3
matheusbach <math.bach.com@gmail.com>
2022-04-25 08:43:47 -0300

    fix cachorro assistindo

--- src/freeRTOS.cpp
+++ src/freeRTOS.cpp
@@ -3,7 +3,7 @@
 
 void CreateRoutineTasks()
 {
-    xTaskCreatePinnedToCore(CheckNetworkConnection, "CheckNetworkConnection", 1024, NULL, 4, NULL, 1);
+    xTaskCreatePinnedToCore(CheckNetworkConnection, "CheckNetworkConnection", 2048, NULL, 4, NULL, 1);
     xTaskCreatePinnedToCore(UpdateClima, "UpdateClima", 2048, NULL, 3, NULL, 1);
     xTaskCreatePinnedToCore(UpdateRSSI, "UpdateRSSI", 1024, NULL, 1, NULL, 1);
   //  xTaskCreatePinnedToCore(DoVentilacaoMinima, "VentilacaoMinima", 1024, NULL, 2, NULL, 0);
