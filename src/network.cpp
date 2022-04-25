c8b0d164e804c4e30a7481bb07e394669dabd178
matheusbach <math.bach.com@gmail.com>
2022-04-25 09:07:09 -0300

    Melhorias no WiFi

--- src/network.cpp
+++ src/network.cpp
@@ -1,15 +1,34 @@
 #include "headers.h"
 
-float RSSI = 100;
+float RSSI = -100;
 const char *ssid = "TendaRouter";
 const char *password = "1234567890";
+
+bool wifiIsConnected = false;
 
 void StartNetWorkConnection()
 {
   WiFi.mode(WIFI_MODE_STA);
+
+  // WiFi.setHostname("Aviario Controle"); // define hostname
+
   WiFi.begin(ssid, password);
+
+  wifi_country_t country = {
+      .cc = "IN",
+      .schan = 1,
+      .nchan = 13,
+      .max_tx_power = 20,
+      .policy = WIFI_COUNTRY_POLICY_AUTO,
+  };
+  esp_wifi_set_country(&country);
+
   WiFi.setAutoConnect(true);
-//  WiFi.setAutoReconnect(true);
+
+  // WiFi.setTxPower(WIFI_POWER_19_5dBm);
+  // WiFi.setAutoReconnect(true);
+
+  Serial.write("wifi tx power: ", WiFi.getTxPower());
 }
 
 void CheckNetworkConnection(void *parameter)
@@ -18,14 +37,19 @@ void CheckNetworkConnection(void *parameter)
   {
     if (!WiFi.isConnected())
     {
-    //  Serial.println("Wifi está desconectado");
+      Serial.println("Wifi desconectado: tentando reconectar");
+      wifiIsConnected = false;
       WiFi.reconnect();
     }
     else{
-    //  Serial.println("Wifi conectado");
+      if (!wifiIsConnected)
+      {
+        wifiIsConnected = true;
+        Serial.println("Conectado ao WiFi");
+      }
     }
 
-    vTaskDelay(10000 / portTICK_PERIOD_MS);
+    vTaskDelay(20000 / portTICK_PERIOD_MS);
   }
 }
 
